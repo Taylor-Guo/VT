86cbc2c6e838ac453c57be2de3283a9b5f99a3f7
Taylor-Guo <499436628@qq.com>
2023-10-22 13:39:44 +0800

    adjust

--- high-level/Commu/Picamera2/capture_circular_stream_R.py
+++ high-level/Commu/Picamera2/capture_circular_stream_R.py
@@ -9,13 +9,13 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import CircularOutput, FileOutput
 
-picam2 = Picamera2()
+picam2 = picamera2.Picamera2()
 video_config = picam2.create_video_configuration({"size": (640, 480), "format": "RGB888"})
 picam2.configure(video_config)
 encoder = H264Encoder(1000000)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.connect(('192.169.137.77', 10001))
+    sock.connect(('10.185.220.245', 10001))
     stream = sock.makefile("wb")
     picam2.start_recording(encoder, FileOutput(stream))
     time.sleep(20)
